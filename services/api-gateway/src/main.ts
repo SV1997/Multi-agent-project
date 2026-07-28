bc64fc7aa5d8dda96825a9697ba4610707aefd18
2024tm93584-spec <2024tm93584@wilp.bits-pilani.ac.in>
2026-07-28 16:57:19 +0530

    revert api-gateway main.ts change, does not belong on frontend branch

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import cookieParser from "cookie-parser"
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -9,7 +9,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  app.use(cookieParser())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
