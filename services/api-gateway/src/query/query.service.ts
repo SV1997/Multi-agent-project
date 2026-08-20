7618d7a23812f14c4c66e470d0f056e8ef85fb4f
2024tm93584-spec <2024tm93584@wilp.bits-pilani.ac.in>
2026-08-20 19:03:19 +0530

    updated for eval and db sync

--- services/api-gateway/src/query/query.service.ts
+++ services/api-gateway/src/query/query.service.ts
@@ -146,6 +146,7 @@ export class QueryService {
     }
 
     async forwardQueryStreamPrismaInitiate(threadId:string,reviewPayload:any,userId: number, domain:string, sessionId: string, turnId:string){
+     
       try {
         const review = await this.prismaService.pendingReview.upsert({
           where:{threadId},
@@ -155,8 +156,8 @@ export class QueryService {
             domain: domain,
             turnId:turnId,
             userId: userId,
-            sources: reviewPayload.answer.sources.map(s=>JSON.stringify(s)),
-            context: reviewPayload.context,
+            sources: reviewPayload.answer.sources.map((s:any)=>JSON.stringify(s)),
+            context: reviewPayload.context.map((c:any)=>JSON.stringify(c)),
             answer: reviewPayload.answer.answer,
             confidence: reviewPayload.answer.confidence,
             resolved: false
@@ -167,7 +168,7 @@ export class QueryService {
             turnId:turnId,
             userId: userId,
             sources: reviewPayload.answer.sources.map(s=>JSON.stringify(s)),
-            context: reviewPayload.context,
+            context: reviewPayload.context.map(c=>JSON.stringify(c)),
             answer: reviewPayload.answer.answer,
             confidence: reviewPayload.answer.confidence,
             resolved: false,
